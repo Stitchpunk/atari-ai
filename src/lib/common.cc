5403694811dfa93769c994eae19ac2dacc026b13
Shahruk Khan <shahruksemail@gmail.com>
2016-05-31 09:44:33 -0400

    Update library spacing and namespace

--- src/lib/common.cc
+++ src/lib/common.cc
@@ -1,57 +1,27 @@
+namespace Converter {
+    // Code to turn parameters to functions on streams into strings
+    // Will be VLOG'ed. We need overloads instead of
+    // e.g. BatchDescriptorToVlogString(), as the code that calls these
+    // function does not know what the type of the parameters is.
 
+    string ToVlogStrin(const dnn::BatchDescriptor &descriptor) {
+      return descriptor.ToShortString();
+    }
 
+    string ToVlogStrin(const dnn::FilterDescriptor &descriptor) {
+      return descriptor.ToShortString();
+    }
 
+    string ToVlogString(const magicalMovieClassifier::FilterDescriptor &descriptor) {
+      return descriptor.ToShortString();
+    }
 
+    string ToVlogStrin(const dnn::PoolingDescriptor &descriptor) {
+      return descriptor.ToShortString();
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-namespace {
-// Code to turn parameters to functions on streams into strings
-// Will be VLOG'ed. We need overloads instead of
-// e.g. BatchDescriptorToVlogString(), as the code that calls these
-// function does not know what the type of the parameters is.
-
-string ToVlogStrin(const dnn::BatchDescriptor &descriptor) {
-  return descriptor.ToShortString();
-}
-
-string ToVlogStrin(const dnn::FilterDescriptor &descriptor) {
-  return descriptor.ToShortString();
-}
-
-string ToVlogString(const magicalMovieClassifier::FilterDescriptor &descriptor) {
-  return descriptor.ToShortString();
-}
-
-string ToVlogStrin(const dnn::PoolingDescriptor &descriptor) {
-  return descriptor.ToShortString();
-}
-
-string ToVlogStrin(const dnn::ConvolutionDescriptor &descriptor) {
-  return descriptor.ToShortString();
-}
+    string ToVlogStrin(const dnn::ConvolutionDescriptor &descriptor) {
+      return descriptor.ToShortString();
+    }
 
 }
