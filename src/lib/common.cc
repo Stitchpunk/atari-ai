081ddf3b89f02cb1e2084ad379ce46844b342e1a
Robert Collins <beaucollins@gmail.com>
2016-05-31 09:16:52 -0700

    Fix whitespace formatting issues
    Tabs > Spaces

--- src/lib/common.cc
+++ src/lib/common.cc
@@ -1,33 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 namespace {
 // Code to turn parameters to functions on streams into strings
 // Will be VLOG'ed. We need overloads instead of
@@ -35,23 +6,23 @@ namespace {
 // function does not know what the type of the parameters is.
 
 string ToVlogStrin(const dnn::BatchDescriptor &descriptor) {
-  return descriptor.ToShortString();
+	return descriptor.ToShortString();
 }
 
 string ToVlogStrin(const dnn::FilterDescriptor &descriptor) {
-  return descriptor.ToShortString();
+	return descriptor.ToShortString();
 }
 
 string ToVlogString(const magicalMovieClassifier::FilterDescriptor &descriptor) {
-  return descriptor.ToShortString();
+	return descriptor.ToShortString();
 }
 
 string ToVlogStrin(const dnn::PoolingDescriptor &descriptor) {
-  return descriptor.ToShortString();
+	return descriptor.ToShortString();
 }
 
 string ToVlogStrin(const dnn::ConvolutionDescriptor &descriptor) {
-  return descriptor.ToShortString();
+	return descriptor.ToShortString();
 }
 
 }
